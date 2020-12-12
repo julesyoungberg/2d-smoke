4b748c949c168025f09cdc645ce7f6704b8847dd
Jules Youngberg <julesdayoungberg@gmail.com>
2020-12-11 20:41:15 -0800

    rainbow color

--- src/FluidConfig.ts
+++ src/FluidConfig.ts
@@ -1,4 +1,11 @@
-import { GUI } from "dat.gui";
+import { GUI } from 'dat.gui';
+import { makeNoise2D } from 'open-simplex-noise';
+
+import { hsvToRgb } from './util/color';
+
+const noise = makeNoise2D(Date.now());
+
+type ColorMode = 'rainbow' | 'static';
 
 export default class FluidConfig {
     gui: GUI;
@@ -17,7 +24,7 @@ export default class FluidConfig {
     gravity = 0; // 100;
     pressure = 0.8;
     // rest temperature of the fluid
-    // 10 for smoke
+    // 10 for smok
     // 15 for clouds
     restTemp = 10;
     temperatureDissipation = 0.01;
@@ -26,13 +33,25 @@ export default class FluidConfig {
     vorticity = 40;
 
     // sim config
+    colorMode: ColorMode = 'rainbow';
+    colorRate = 1;
+    colorOffset = 0;
     pressureIterations = 50;
     splatRadius = 0.1;
     splatForce = 6000;
+    noiseX = 0.0;
 
     constructor(gui: GUI) {
         this.gui = gui;
+        this.colorOffset = Math.random();
 
+        gui.add(this, 'colorMode', ['rainbow', 'static']).onChange((val) => {
+            if (val === 'static') {
+                this.color = this.getCurrentColor().map(c => c * 255 * 50);
+            }
+            gui.updateDisplay();
+        });
+        gui.add(this, 'colorRate', 0, 10);
         gui.addColor(this, 'color');
 
         gui.add(this, 'densityDissipation', 0, 0.5);
@@ -52,7 +71,18 @@ export default class FluidConfig {
         gui.add(this, 'splatForce', 0, 10000);
     }
 
+    getCurrentColor() {
+        const n = (noise(this.noiseX, 0) + 1) * 0.5;
+        this.noiseX += this.colorRate / 40000;
+        return hsvToRgb((n * 3 + this.colorOffset) % 1, 0.8, 0.5).map(c => c / 40);
+    }
+
     getColor() {
-        return this.color.map(c => c / (255 * 50));
+        switch(this.colorMode) {
+            case 'rainbow':
+                return this.getCurrentColor();
+            default:
+                return this.color.map(c => c / (255 * 50));
+        }
     }
 }
